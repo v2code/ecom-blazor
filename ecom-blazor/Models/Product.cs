988b0638f3e7ecc6f0d40e959ed16deedc937ec4
Vishal Vaishnav <vishal.jsk@gmail.com>
2025-04-02 15:15:21 -0700

    swapped project name,  quick data binding test

---
+++ ecom-blazor/Models/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace ecomblazor.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ ecomblazor/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace ecomblazor.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
