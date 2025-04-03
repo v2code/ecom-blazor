6bb31f049ef7ef269c9ec566fe7b9105662e6e45
Vishal Vaishnav <vishal.jsk@gmail.com>
2025-04-02 17:23:31 -0700

    checkbox binding

--- ecomblazor/Models/Product.cs
+++ ecomblazor/Models/Product.cs
@@ -6,5 +6,6 @@
         public string Name { get; set; }
         public double Price { get; set; }
         public bool IsActive { get; set; }
+        public IEnumerable<ProductProp> Properties { get; set; }
     }
 }
---
+++ ecomblazor/Models/ProductProp.cs
@@ -0,0 +1,10 @@
+﻿namespace ecomblazor.Models
+{
+    public class ProductProp
+    {
+        public int id { get; set; }
+        public string Key { get; set; }
+
+        public string Value { get; set; }
+    }
+}
